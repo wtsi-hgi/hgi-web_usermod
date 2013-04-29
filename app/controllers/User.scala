329deb692217a5d5eb05c1136c8cdde6d2f5ff19
Nicholas Clarke <nc6@sanger.ac.uk>
2013-04-29 10:24:01 +0100

    Ability to add roles to users.

---
+++ app/controllers/User.scala
@@ -0,0 +1,41 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json.Json
+import models._
+import play.api.libs.json.JsError
+
+object User extends Controller {
+
+  def all() = Action {
+    val users = models.User.all().map(a => Json.toJson(a))
+    val json = Json.obj("users" -> users)
+
+    Ok(json)
+  }
+
+  def get(sid: String) = Action {
+    val user = models.User.get(sid).map(a => Json.toJson(a))
+    user.map(Ok(_)).getOrElse(NotFound("No user with sid " + sid + " is registered in the system."))
+  }
+
+  def roles(sid: String) = Action {
+    val roles = models.User.roles(sid).map(a => Json.toJson(a))
+    val json = Json.obj("roles" -> roles)
+
+    Ok(json)
+  }
+
+  def addRole(sid: String) = Action(parse.json) { request =>
+    request.body.validate[Role].map { role =>
+      models.User.addRole(sid,role) match {
+        case Right(id) => Ok(Json.obj("id" -> id))
+        case Left(errs) => Forbidden(Json.arr(errs))
+      }
+    }.recoverTotal { jsErr =>
+      BadRequest(JsError.toFlatJson(jsErr))
+    }
+  }
+
+}
--- app/models/User.scala
+++ app/models/User.scala
@@ -6,6 +6,7 @@ import play.api.db.slick.Config.driver.simple._
 import play.api.db.slick.DB
 import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import play.api.libs.json.Json
+import scala.util.control.Exception.nonFatalCatch
 
 case class User(sid: String)
 object User {
@@ -24,5 +25,23 @@ object User {
   def roles(sid: String) = DB.withSession { implicit session =>
     val rdos = Query(Users).filter(_.sid === sid).flatMap(_.roles).list
     rdos.map(rdo => Role.get(rdo.id))
+  }
+
+  // Insert role, insert user-role
+  /**
+   * @return either Right(id) of the new role, or Left(errors).
+   */
+  def addRole(sid: String, role: Role) = DB.withSession { implicit session =>
+    
+    def insertUserRole(user : Long, role : Long) = {
+      val either = nonFatalCatch.either(models.dao.UserRoles.insert((user, role)))
+      either.left.map(a => Seq(a.getMessage()))      
+    }
+    
+    for {
+      user <- Query(Users).filter(_.sid === sid).firstOption.toRight(Seq("User " + sid + " does not exist in the system.")).right
+      insertedRole <- models.Role.insert(role).right
+      insertedUserRole <- insertUserRole(user.id, insertedRole).right
+    } yield insertedRole
   }
 }

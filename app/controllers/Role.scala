b41dfd6f4f1b65744fe63e3e3291d044137f493c
Nicholas Clarke <nc6@sanger.ac.uk>
2013-04-29 14:50:20 +0100

    Can now look up users for a given role.
    Closes issue #2.

--- app/controllers/Role.scala
+++ app/controllers/Role.scala
@@ -5,11 +5,12 @@ import play.api.mvc._
 import play.api.libs.json.Json
 import models._
 import play.api.libs.json.JsError
+import scala.util.parsing.combinator.RegexParsers
 
 object Role extends Controller {
 
   def all() = Action {
-    val roles = models.Role.all().map(a => Json.toJson(a))
+    val roles = Json.toJson(models.Role.all())
     val json = Json.obj("roles" -> roles)
 
     Ok(json)
@@ -25,6 +26,36 @@ object Role extends Controller {
     }.recoverTotal { jsErr =>
       BadRequest(JsError.toFlatJson(jsErr))
     }
+  }
+
+  /**
+   * Look up a specific role.
+   *
+   * @param role The role parameter will be encoded in the form rolename;(paramname=paramvalue,)*
+   */
+  def users(role: String) = Action {
+    URIParameters.parse(role) match {
+      case URIParameters.Success((name, keyvals), _) => {
+        val role = models.Role(name, keyvals map (Function.tupled(Parameter.apply)))
+        val users = models.Role.users(role)
+        Ok(Json.toJson(users))
+      }
+      case URIParameters.Failure(msg, _) => BadRequest(s"Parse failure: $msg \n Input: $role")
+      case URIParameters.Error(msg, _) => BadRequest(s"Parse error: $msg \n Input: $role")
+    }
+  }
+
+  /**
+   * Parser for path segments using foo;bar=baz syntax
+   */
+  object URIParameters extends RegexParsers {
+    override type Elem = Char
+    def part = """[a-zA-Z0-9_-]*""".r
+    def keyval = part ~ "=" ~ part ^^ { case k ~ _ ~ v => (k, v) }
+    def keyvals = repsep(keyval, ",")
+    def whole = part ~ opt("[;&]".r ~> keyvals) ^^ { case n ~ v => (n, v.getOrElse(Seq())) }
+
+    def parse(s: String) = parseAll(whole, s)
   }
 
 }
--- app/models/Role.scala
+++ app/models/Role.scala
@@ -37,34 +37,71 @@ object Role {
   }
 
   /**
+   * Get the id for the specified role.
+   */
+  private[models] def find(role: Role) = DB.withSession { implicit session =>
+
+    // Constructing a query for the parameters is annoying, so we get all roles and then filter
+    val roleQ = for {
+      ((r, p), pt) <- Roles leftJoin dao.Parameters on (_.id === _.roleId) leftJoin dao.ParameterTypes on (_._2.ptId === _.id)
+      rt <- r.roleType if rt.name === role.name
+    } yield (r.id, pt.name.?, p.value.?)
+
+    val roles = roleQ.list.groupBy(_._1).map {
+      case (a, b) =>
+        (a, b.collect { case (_, Some(n), Some(v)) => Parameter(n, v) })
+    }.filter(r => r._2 == role.parameters)
+
+    // There should be precisely one role
+    roles.map(_._1).headOption
+  }
+
+  /**
+   * Get all users with the given role.
+   */
+  def users(role: Role) = DB.withSession { implicit session =>
+    find(role) match {
+      case Some(id) => Query(UserRoles).filter(_.roleId === id).flatMap(_.user).list.map(u => User(u.sid))
+      case None => Seq()
+    }
+  }
+
+  /**
    * Insert a new role. This will fail if either the role type or any parameter type does not exist,
    * or if the parameters are not appropriate to that role. It will also fail if not all parameters have been provided.
+   *
+   * If the role already exists, just return the id of that.
    *
    * @Return either Right(id) of the new role, or Left(message) that describes the problem.
    */
   def insert(role: Role): Either[Seq[String], Long] = DB.withSession { implicit session =>
-    val q = Query(RoleTypes).filter(_.name === role.name)
-    val roleType = q.firstOption.toRight("Cannot add role for missing role type: " + role.name)
-    val parameterIdMap = q.flatMap(_.parameters).map(a => a.name -> a.id).list.toMap
+    find(role) match {
+      case Some(id) => Right(id)
+      case None => {
+        val q = Query(RoleTypes).filter(_.name === role.name)
+        val roleType = q.firstOption.toRight("Cannot add role for missing role type: " + role.name)
+        val parameterIdMap = q.flatMap(_.parameters).map(a => a.name -> a.id).list.toMap
 
-    val providedParameters = role.parameters.map(_.name).toSet
-    val missingParameters = (parameterIdMap.keySet -- providedParameters)
-    val surplusParameters = providedParameters -- parameterIdMap.keySet
+        val providedParameters = role.parameters.map(_.name).toSet
+        val missingParameters = (parameterIdMap.keySet -- providedParameters)
+        val surplusParameters = providedParameters -- parameterIdMap.keySet
 
-    val ids = (roleType, missingParameters, surplusParameters) match {
-      case (Right(roleType), m, s) if (m.isEmpty && s.isEmpty) => Right(
-        (roleType.id, role.parameters map (p => parameterIdMap(p.name) -> p.value)))
-      case (r, m, s) => Left(
-        r.left.toSeq ++:
-          m.toSeq.map("Missing parameter value for parameter type: " + _) ++:
-          s.toSeq.map("Cannot add parameter for missing parameter type: " + _))
-    }
+        val ids = (roleType, missingParameters, surplusParameters) match {
+          case (Right(roleType), m, s) if (m.isEmpty && s.isEmpty) => Right(
+            (roleType.id, role.parameters map (p => parameterIdMap(p.name) -> p.value)))
+          case (r, m, s) => Left(
+            r.left.toSeq ++:
+              m.toSeq.map("Missing parameter value for parameter type: " + _) ++:
+              s.toSeq.map("Cannot add parameter for missing parameter type: " + _))
+        }
 
-    ids.right.map {
-      case (roleTypeId, parameters) => {
-        val roleId = dao.Roles.forInsert insert roleTypeId
-        parameters.foreach { case (id, value) => dao.Parameters.forInsert insert (roleId, id, value) }
-        roleId
+        ids.right.map {
+          case (roleTypeId, parameters) => {
+            val roleId = dao.Roles.forInsert insert roleTypeId
+            parameters.foreach { case (id, value) => dao.Parameters.forInsert insert (roleId, id, value) }
+            roleId
+          }
+        }
       }
     }
   }

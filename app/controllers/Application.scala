5118ad69c72addd8b175a41f7c8e1b0daf7c8240
Nicholas Clarke <nc6@sanger.ac.uk>
2013-05-14 15:23:14 +0100

    Add a testAuthentication method.
    GET /testAuthenticate requires authentication (but no other details)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import global.Authenticated.authenticated
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def testAuth = authenticated { user =>
+    Action {
+      Ok(s"You have authenticated successfully, $user.")
+    }
+  }
+
 }

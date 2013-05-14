0d60da0b65269d86f7b5bf44aaee309fd55c49c1
Nicholas Clarke <nc6@sanger.ac.uk>
2013-05-14 12:59:14 +0100

    Fix the initial data scripts.
    This should now correctly grant the initial user set_global_role permissions, and should be database independent.Also fix the evolution script to re-enable DDL generation.

--- app/models/InitialData.scala
+++ app/models/InitialData.scala
@@ -3,22 +3,28 @@ package models
 import play.api._
 import models._
 import models.dao._
+import rules.MetaRoles
 
 object InitialData {
+
+  val set_global_role = MetaRoles.set_global_role.instantiate(Map.empty[String, String]).get
 
   val initialParameterTypes = Seq(
     ParameterType("project", Some("Project role")))
 
   val initialRoleTypes = Seq(
-    RoleType("create_project", Some("Can create projects."), Nil),
-    RoleType("set_global_role", Some("Can set roles globally."), Nil),
-    RoleType("delegate", Some("Can delegate permissions for this project."), initialParameterTypes),
+    MetaRoles.set_global_role,
+    MetaRoles.create_project,
+    MetaRoles.delegate,
     RoleType("manage_project_users", Some("Can manage users for this project."), initialParameterTypes),
     RoleType("manage_project_datasets", Some("Can manage datasets for this project."), initialParameterTypes),
     RoleType("manage_project_resources", Some("Can manage resources for this project."), initialParameterTypes))
-    
+
   val initialUsers = Seq(
     UserDO(1L, "nc6@sanger.ac.uk"))
+
+  val initialUserRoles = Seq(
+    "nc6@sanger.ac.uk" -> set_global_role)
 
   def insert() {
     if (ParameterTypes.count == 0 && RoleTypes.count == 0) {
@@ -27,7 +33,7 @@ object InitialData {
 
     if (Users.count == 0) {
       initialUsers.foreach(Users.insert)
-      initialUsers.foreach(a => User.addRole(a.sid, initialRoleTypes.head.instantiate(Nil : Seq[Parameter]).get))
+      initialUserRoles.foreach(Function.tupled(User.addRole))
     }
   }
 

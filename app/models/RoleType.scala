a8cb774ab0a6f3ed02a5952dd9397893bf54c0de
Nicholas Clarke <nc6@sanger.ac.uk>
2014-04-04 14:06:57 +0100

    Add license info to all files.

--- app/models/RoleType.scala
+++ app/models/RoleType.scala
@@ -1,3 +1,36 @@
+/*
+Copyright (c) 2013, Wellcome Trust Sanger Institute
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of the Wellcome Trust Sanger Institute nor the 
+      names of other contributors may be used to endorse or promote 
+      products derived from this software without specific prior written 
+      permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package models
 
 import play.api.libs.json._
@@ -67,10 +100,16 @@ object RoleType {
     // Need to kill all parameters + roles using this role type.
     val roleType = Query(RoleTypes).filter(_.name === role.name)
     val roles = roleType flatMap (a => Query(Roles).filter(_.rtId === a.id));
-
+    {
+      val ids = roleType.map(_.id).list
+      for (id <- ids) {
+        Query(RoleTypeParameterTypes).filter(_.rtId === id).delete
+      }
+    }
     {
       val ids = roles.map(_.id).list
       for (id <- ids) {
+        Query(dao.UserRoles).filter(_.roleId === id).delete
         Query(dao.Parameters).filter(_.roleId === id).delete
         Query(dao.Roles).filter(_.id === id).delete
       }
